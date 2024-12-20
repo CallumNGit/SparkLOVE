6fe7e1b656345388bfcb27f892a3be61d991191b
CallumNGit <109044018+CallumNGit@users.noreply.github.com>
2024-12-20 17:24:19 +0000

    Initial code

---
+++ SparkLÖVE/conf.lua
@@ -0,0 +1,8 @@
+-- CONFIG FUNCTION
+function love.conf(t)
+    t.window.title = "SparkLÖVE"
+    t.window.icon = "assets/window-icon.png"
+    t.window.width = 1600
+    t.window.height = 900
+    t.window.resizable = true
+end
---
+++ SparkLÖVE/main.lua
@@ -0,0 +1,14 @@
+-- LOAD FUNCTION
+function love.load()
+    love.graphics.setBackgroundColor(0.95, 0.95, 0.95)
+end
+
+-- UPDATE FUNCTION
+function love.update(dt)
+
+end
+
+-- DRAW FUNCTION
+function love.draw()
+
+end
